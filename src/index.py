4c9bb8ce43fde0be7bd726d314c62696d8809db9
lvxiaoliang <lvxiaoliang@bilibili.com>
2021-05-28 06:02:16 +0000

    base frame

---
+++ src/index.py
@@ -0,0 +1,27 @@
+import sys
+from types import rss_type
+
+# 设定本地存储 
+class RSS3 :
+    def __init_(self) :
+        pass
+
+    def profilePatch(IRSS3Profile) :
+        # 从本地存储中找到合适的profile并修改
+        pass
+    
+    def itemPost() :
+
+        pass
+
+    def itemsPatch() :
+        pass
+
+    def getFile() :
+        pass
+
+    def delFile() :
+        pass
+
+    def sign() :
+        pass
